aca441e68adbe86011cb8b52dbbd63446ad0788f
Shubham Jain <someshubham@gmail.com>
2026-09-20 21:57:17 +0530

    feature: add post req res

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
+
+type errorRes struct {
+	Error string `json:"error"`
+}
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
@@ -60,6 +66,60 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricHandler())
 	mux.HandleFunc("POST /admin/reset", apiCfg.metricReset())
 
+	// FE facing
 	mux.Handle("/app", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	// validate_chirp
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, req *http.Request) {
+		type param struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(req.Body)
+		prm := param{}
+		err := decoder.Decode(&prm)
+		w.Header().Set("Content-Type", "application/json")
+		if err != nil {
+			writeError(w, "Something went wrong")
+			return
+		}
+
+		if len(prm.Body) > 140 {
+			writeError(w, "Chirp is too long")
+			return
+		}
+
+		type returnVal struct {
+			Valid bool `json:"valid"`
+		}
+
+		resBody := returnVal{
+			Valid: true,
+		}
+
+		dat, err := json.Marshal(resBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(200)
+		w.Write(dat)
+
+	})
+
 	server.ListenAndServe()
+}
+
+func writeError(w http.ResponseWriter, msg string) {
+	errResp := errorRes{
+		Error: msg,
+	}
+	dat, err := json.Marshal(errResp)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(400)
+	w.Write(dat)
 }
