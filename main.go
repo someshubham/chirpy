91d03c7f8a89440c481b339e525d3ea86d05cbf7
Shubham Jain <someshubham@gmail.com>
2026-09-21 23:42:41 +0530

    feature: add delete create user api

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type errorRes struct {
 }
 
 type apiConfig struct {
+	platform       string
 	fileServerHits atomic.Int32
 	db             *database.Queries
 }
@@ -52,6 +53,63 @@ func (a *apiConfig) metricHandler() func(w http.ResponseWriter, r *http.Request)
 func (a *apiConfig) metricReset() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		a.fileServerHits.Store(0)
+		if strings.Compare(a.platform, "dev") != 0 {
+			w.WriteHeader(403)
+			return
+		}
+		err := a.db.DeleteAllUsers(r.Context())
+		if err != nil {
+			fmt.Println("Unable to delete records")
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(200)
+	}
+}
+
+func (a *apiConfig) addUsers() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		type param struct {
+			Email string `json:"email"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		prm := param{}
+		err := decoder.Decode(&prm)
+		w.Header().Set("Content-Type", "application/json")
+		if err != nil {
+			writeError(w, "Something went wrong")
+			return
+		}
+
+		usr, err := a.db.CreateUser(r.Context(), prm.Email)
+		if err != nil {
+			writeError(w, "Unable to create a user")
+			return
+		}
+
+		type returnVal struct {
+			ID        string `json:"id"`
+			CreatedAt string `json:"created_at"`
+			UpdatedAt string `json:"updated_at"`
+			Email     string `json:"email"`
+		}
+
+		val := returnVal{
+			ID:        usr.ID.String(),
+			CreatedAt: usr.CreatedAt.String(),
+			UpdatedAt: usr.UpdatedAt.String(),
+			Email:     usr.Email,
+		}
+
+		dat, err := json.Marshal(val)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(201)
+		w.Write(dat)
 	}
 }
 
@@ -63,6 +121,7 @@ func main() {
 	}
 
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
@@ -73,7 +132,8 @@ func main() {
 	dbQueries := database.New(db)
 
 	apiCfg := apiConfig{
-		db: dbQueries,
+		db:       dbQueries,
+		platform: platform,
 	}
 
 	mux := http.NewServeMux()
@@ -90,6 +150,7 @@ func main() {
 	})
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricHandler())
 	mux.HandleFunc("POST /admin/reset", apiCfg.metricReset())
+	mux.HandleFunc("POST /api/users", apiCfg.addUsers())
 
 	// FE facing
 	mux.Handle("/app", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
@@ -132,7 +193,6 @@ func main() {
 		}
 		w.WriteHeader(200)
 		w.Write(dat)
-
 	})
 
 	server.ListenAndServe()
