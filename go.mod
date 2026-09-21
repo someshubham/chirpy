d88dab440b4daf6f8ef53b80c364e5c6830e5645
Shubham Jain <someshubham@gmail.com>
2026-09-21 23:07:00 +0530

    feature: add db support

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.12.3
 )
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/someshubham/chirpy/internal/database"
 )
 
 type errorRes struct {
@@ -17,6 +21,7 @@ type errorRes struct {
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	db             *database.Queries
 }
 
 func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -51,8 +56,25 @@ func (a *apiConfig) metricReset() func(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("unable to load .env file")
+		return
+	}
 
-	apiCfg := apiConfig{}
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Unable to open db\n%s\n", err.Error())
+		return
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		db: dbQueries,
+	}
 
 	mux := http.NewServeMux()
 
